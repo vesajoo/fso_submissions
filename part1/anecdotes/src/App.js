c3778a075b3bb7268df34b1795f65f32d87f201c
vesajoo <joonasvesa0@gmail.com>
2023-09-18 11:26:17 +0300

    anecdotes finished

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -6,6 +6,18 @@ const Button = ({handleClick, text}) => (
   </button>
 )
 
+const Title = (props) => (
+  <h1>{props.text}</h1>
+)
+
+const Anecdote = (props) => (
+  <p>{props.text}</p>
+)
+
+const Votes = (props) => (
+  <p>has {props.value} votes</p>
+)
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -17,18 +29,36 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
+  const initialPoints = Array(anecdotes.length).fill(0)
+
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(initialPoints)
+  const [mostVotes, setMostVotes] = useState("No votes yet")
 
   const handleRandomClick = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
 
+  const handleVoteClick = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+    if (Math.max(...points) < copy[selected]) {
+      setMostVotes([anecdotes[selected], copy[selected]])
+    }
+  }
+  
   return (
     <div>
-      {anecdotes[selected]}
-      <br></br>
+      <Title text='Anecdote of the day'></Title>
+      <Anecdote text={anecdotes[selected]}></Anecdote>
+      <Votes value={points[selected]}></Votes>
+      <Button handleClick={handleVoteClick} text='vote'/>
       <Button handleClick={handleRandomClick} text='next anecdote'/>
+      <Title text='Anecdote with the most votes'></Title>
+      <Anecdote text={mostVotes[0]}></Anecdote>
+      <Votes value={mostVotes[1]}></Votes>
     </div>
   )
 }
