8df79b7b64a6e23b6df01250d30be15c69407d5b
vesajoo <joonasvesa0@gmail.com>
2023-09-29 14:35:18 +0300

    Fixed courses not given key

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -49,7 +49,7 @@ const App = () => {
     <div>
       <h1>Web development curriculum</h1>
       {courses.map(course=>
-        <Course course={course} />
+        <Course key={course.id} course={course} />
       )}
     </div>
   ) 
